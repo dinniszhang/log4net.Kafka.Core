eb7123f860edb3161e0cf9e4385a51f8aa4c6571
zhangchunyu <459073458@qq.com>
2021-08-03 10:29:01 +0800

    修复命名空间。

--- KafkaLogHelper.cs
+++ KafkaLogHelper.cs
@@ -6,7 +6,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Text;
 
-namespace cyqf_api_diebiangaokao.Tools
+namespace log4net.Kafka.Core
 {
     /// <summary>
     ///  KafkaLogHelper类
